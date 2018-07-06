b1a60af111b15ae257d125a335f57f03ad85fb8d
Guy Babbitt <babbigu@fsafood.net>
2018-07-05 18:55:01 -0700

    Final draft

---
+++ src/util/Yelp.js
@@ -0,0 +1,31 @@
+const apiKey = '4Li_MaRwjghYKF0svIk3JSS-U6wVjINMJARujgB8KAmPZ4KR1BomrntGGueKcDcZPVj0tenM3tpA-UU3grfUyRIRDtuo2Qhr_sOyDHZq7--8XR5KjNo6_VnjE5A-W3Yx';
+const Yelp = {
+    search(term, location, sortBy) {
+        const endpoint = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+        console.log(endpoint);
+        return fetch(endpoint, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => ({
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count,
+                }));
+            }
+        });
+    }
+};
+
+export default Yelp;
